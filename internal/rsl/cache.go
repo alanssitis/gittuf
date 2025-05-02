da0824c8dae3fda6da0fd4bced6a4fa4100ffe84
Alan Chung Ma <alan@chung-ma.com>
2025-05-02 17:45:59 -0400

    add RWMutex to internal/rsl/cache.go
    Signed-off-by: Alan Chung Ma <alan@chung-ma.com>

--- internal/rsl/cache.go
+++ internal/rsl/cache.go
@@ -3,23 +3,39 @@
 
 package rsl
 
-import "github.com/gittuf/gittuf/internal/gitinterface"
+import (
+	"sync"
+
+	"github.com/gittuf/gittuf/internal/gitinterface"
+)
 
 type rslCache struct {
 	entryCache  map[string]Entry
 	parentCache map[string]string
+
+	ecMu sync.RWMutex
+	pcMu sync.RWMutex
 }
 
 func (r *rslCache) getEntry(id gitinterface.Hash) (Entry, bool) {
+	r.ecMu.RLock()
+	defer r.ecMu.RUnlock()
+
 	entry, has := r.entryCache[id.String()]
 	return entry, has
 }
 
 func (r *rslCache) setEntry(id gitinterface.Hash, entry Entry) {
+	r.ecMu.Lock()
+	defer r.ecMu.Unlock()
+
 	r.entryCache[id.String()] = entry
 }
 
 func (r *rslCache) getParent(id gitinterface.Hash) (gitinterface.Hash, bool, error) {
+	r.pcMu.RLock()
+	defer r.pcMu.RUnlock()
+
 	parentID, has := r.parentCache[id.String()]
 	if !has {
 		return nil, false, nil
@@ -33,6 +49,9 @@ func (r *rslCache) getParent(id gitinterface.Hash) (gitinterface.Hash, bool, err
 }
 
 func (r *rslCache) setParent(id, parentID gitinterface.Hash) {
+	r.pcMu.Lock()
+	defer r.pcMu.Unlock()
+
 	r.parentCache[id.String()] = parentID.String()
 }
 
